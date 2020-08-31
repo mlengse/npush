3d61cc3c8224e32f8fca406353e82f26831639a8
mlengse <anjangkn@gmail.com>
2020-08-31 10:45:44 +0700

    logs

--- core/lib/spinner.js
+++ core/lib/spinner.js
@@ -2,10 +2,10 @@ const ora = require('ora')
 exports.spinner =(process.platform === 'win32' && !process.env.NODE_APP_INSTANCE) ? ora({
   stream: process.stdout
 }): {
-  start: text => console.log(text),
+  start: text => console.log(`start: ${text}`),
   stop: _ => '',
-  succeed: text => console.log(text),
-  warn: text => console.info(text),
-  info: text => console.info(text),
-  fail: text => console.error(text)
+  succeed: text => console.log(`done: ${text}`),
+  warn: text => console.info(`warn: ${text}`),
+  info: text => console.info(`info: ${text}`),
+  fail: text => console.error(`error: ${text}`)
 }
