45f68d3a34212806865d6897f3010d6a3a680956
mlengse <anjangkn@gmail.com>
2021-02-02 13:03:12 +0700

    asdad

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { schedule } = require('node-cron')
 const init = require('./init')
 const menit = Math.floor(Math.random() * 60)
 const jamPagi = 1 + Math.floor(Math.random() * 6)
-const jamSore = 15 + Math.floor(Math.random() * 6)
+const jamSore = 13 + Math.floor(Math.random() * 6)
 
 console.log(`set cron: ${menit} ${jamPagi},${jamSore} * * * ${new Date()}`)
 
