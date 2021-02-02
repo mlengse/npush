0954f9569bd78ec2a7c12101042d3b34be1a1366
mlengse <anjangkn@gmail.com>
2021-02-02 14:38:16 +0700

    sdf

--- core/lib/time.js
+++ core/lib/time.js
@@ -1,27 +1,26 @@
 const moment = require('moment')
-const momentTS = require('moment')
-exports.xTimestamp = () => momentTS.utc().format('X')
-moment.now = () => +new Date('2021', '0', '30');
-exports.tgl = () => moment().add(-1, 'd').date()
-exports.blnThn = () => moment().add(-1, 'd').format('MM-YYYY')
+exports.xTimestamp = () => moment.utc().format('X')
+// moment.now = () => +new Date('2021', '0', '30');
+exports.tgl = () => moment('31-01-2021', 'DD-MM-YYYY').add(-1, 'd').date()
+exports.blnThn = () => moment('31-01-2021', 'DD-MM-YYYY').add(-1, 'd').format('MM-YYYY')
 exports.tglHariIni = () => `${this.tgl()}-${this.blnThn()}`
-exports.now = moment().format('D')
-exports.end = moment().endOf('month').format('D')
-exports.blnThnGetPst = () => moment().add(-3, 'month').format('MM-YYYY')
-exports.tglBlnLalu = () => moment().add(-1, 'month').format('D-MM-YYYY')
+exports.now = moment('31-01-2021', 'DD-MM-YYYY').format('D')
+exports.end = moment('31-01-2021', 'DD-MM-YYYY').endOf('month').format('D')
+exports.blnThnGetPst = () => moment('31-01-2021', 'DD-MM-YYYY').add(-3, 'month').format('MM-YYYY')
+exports.tglBlnLalu = () => moment('31-01-2021', 'DD-MM-YYYY').add(-1, 'month').format('D-MM-YYYY')
 exports.tglKmrn = tgl  => moment(tgl, 'D-MM-YYYY').clone().add(-1,'d').format('D-MM-YYYY')
 exports.tglDaftar = () => {
- if(moment().day() === 0){
-  if(moment().add(-4, 'd').day() === 0){
-    return moment().add(-3, 'd').format('DD-MM-YYYY')
+ if(moment('31-01-2021', 'DD-MM-YYYY').day() === 0){
+  if(moment('31-01-2021', 'DD-MM-YYYY').add(-4, 'd').day() === 0){
+    return moment('31-01-2021', 'DD-MM-YYYY').add(-3, 'd').format('DD-MM-YYYY')
   } else {
-    return moment().add(-4, 'd').format('DD-MM-YYYY')
+    return moment('31-01-2021', 'DD-MM-YYYY').add(-4, 'd').format('DD-MM-YYYY')
   }
  } else {
-  if(moment().add(-3, 'd').day() === 0){
-    return moment().add(-2, 'd').format('DD-MM-YYYY')
+  if(moment('31-01-2021', 'DD-MM-YYYY').add(-3, 'd').day() === 0){
+    return moment('31-01-2021', 'DD-MM-YYYY').add(-2, 'd').format('DD-MM-YYYY')
   } else {
-    return moment().add(-3, 'd').format('DD-MM-YYYY')
+    return moment('31-01-2021', 'DD-MM-YYYY').add(-3, 'd').format('DD-MM-YYYY')
   }
  } 
 } 
