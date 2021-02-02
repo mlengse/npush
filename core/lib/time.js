547d9afb9d61884845d318cd71cf28140270d911
mlengse <anjangkn@gmail.com>
2021-02-02 14:33:27 +0700

    asd

--- core/lib/time.js
+++ core/lib/time.js
@@ -1,9 +1,9 @@
 const moment = require('moment')
+exports.xTimestamp = () => moment.utc().format('X')
 moment.now = () => +new Date('2021', '0', '30');
 exports.tgl = () => moment().add(-1, 'd').date()
 exports.blnThn = () => moment().add(-1, 'd').format('MM-YYYY')
 exports.tglHariIni = () => `${this.tgl()}-${this.blnThn()}`
-exports.xTimestamp = () => moment.utc().format('X')
 exports.now = moment().format('D')
 exports.end = moment().endOf('month').format('D')
 exports.blnThnGetPst = () => moment().add(-3, 'month').format('MM-YYYY')
