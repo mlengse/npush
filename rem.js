1225688ad51569796741df63f8f9c9f560b921c8
mlengse <anjangkn@gmail.com>
2021-03-22 11:41:28 +0700

    tes drive

--- rem.js
+++ rem.js
@@ -9,44 +9,45 @@ const app = new Core(config)
 module.exports = async (isPM2) => {
 
   try{
-    await app.init()
+    // await app.init()
+    await app.listKontak()
 
-    app.dataBBTB = []
-    app.cekPstSudah =[]
+    // app.dataBBTB = []
+    // app.cekPstSudah =[]
 
-    let kunjIni = (await app.getPendaftaranProvider({
-      tanggal: '20-12-2020'
-    }))
-    .filter(({
-      kunjSakit,
-      status
-    }) => kunjSakit && status )
-    .map(({
-      noUrut, 
-      tgldaftar, 
-      peserta: { 
-        noKartu 
-      },
-      poli: {
-        kdPoli
-      }
-    }) => ({
-      noUrut,
-      noKartu,
-      tgldaftar,
-      kdPoli
-    }))
+    // let kunjIni = (await app.getPendaftaranProvider({
+    //   tanggal: '20-12-2020'
+    // }))
+    // .filter(({
+    //   kunjSakit,
+    //   status
+    // }) => kunjSakit && status )
+    // .map(({
+    //   noUrut, 
+    //   tgldaftar, 
+    //   peserta: { 
+    //     noKartu 
+    //   },
+    //   poli: {
+    //     kdPoli
+    //   }
+    // }) => ({
+    //   noUrut,
+    //   noKartu,
+    //   tgldaftar,
+    //   kdPoli
+    // }))
 
-    console.log(kunjIni.length)
+    // console.log(kunjIni.length)
 
-    for( let kunj of kunjIni){
-      console.log(kunj)
-      let re = await app.deletePendaftaran({
-        ...kunj
-      })
+    // for( let kunj of kunjIni){
+    //   console.log(kunj)
+    //   let re = await app.deletePendaftaran({
+    //     ...kunj
+    //   })
 
-      console.log(re)
-    }
+    //   console.log(re)
+    // }
 
 
     await app.close(isPM2)
