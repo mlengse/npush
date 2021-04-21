988ea09e9402373c6e9ad1b18382e2f7e4a9ebb2
mlengse <anjangkn@gmail.com>
2021-04-22 06:23:45 +0700

    lnk

--- core/lib/spinner.js
+++ core/lib/spinner.js
@@ -1,12 +1,17 @@
 const ora = require('ora')
-
-console.log(process.platform === 'win32' && !process.env.NODE_APP_INSTANCE)
-console.log(process.platform === 'win32')
-console.log(!process.env.NODE_APP_INSTANCE)
+let textS = ''
+// console.log(process.platform === 'win32' && !process.env.NODE_APP_INSTANCE)
+// console.log(process.platform === 'win32')
+// console.log(!process.env.NODE_APP_INSTANCE)
 exports.spinner =(!process.env.NODE_APP_INSTANCE) ? ora({
   stream: process.stdout
 }): {
-  start: text => '',// console.log(`start: ${text}`),
+  start: text => {
+    if(textS !== text) {
+      textS = text
+      console.log(`start: ${text}`)
+    }
+  },
   stop: _ => '',
   succeed: text => console.log(`done: ${text}`),
   warn: text => console.info(`warn: ${text}`),
