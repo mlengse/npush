ca138393f940f13e2281c006de0226150ed766f7
mlengse <anjangkn@gmail.com>
2021-04-22 06:15:10 +0700

    vfd

--- core/index.js
+++ core/index.js
@@ -33,8 +33,8 @@ module.exports = class Core {
     let settings = await this.getSettings()
     let dokter = await this.getDokters()
 
-    console.log(settings)
-    console.log(dokter)
+    // console.log(settings)
+    // console.log(dokter)
 
     if(settings.length){
       this.config = Object.assign({}, this.config, {
