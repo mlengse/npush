0ae5d2636d1ed32d76b454ff8239dedc46aae1b0
mlengse <anjangkn@gmail.com>
2021-05-24 13:54:24 +0700

    asdad

--- main.js
+++ main.js
@@ -101,9 +101,9 @@ module.exports = async (isPM2) => {
             if((re.diagnosa1.kdDiag === 'E11.9' || re.diagnosa2.kdDiag === 'E11.9' || re.diagnosa3.kdDiag === 'E11.9'
             || re.diagnosa1.kdDiag === 'E11' || re.diagnosa2.kdDiag === 'E11' || re.diagnosa3.kdDiag === 'E11'
             ) ){
-              // console.log('')
-              // console.log('-------------')
-              // console.log('is DM: ', JSON.stringify(re))
+              console.log('')
+              console.log('-------------')
+              console.log('is DM: ', JSON.stringify(re))
               isDM = true
               let mcu = await app.getMCU({
                 noKunjungan: re.noKunjungan
@@ -112,9 +112,9 @@ module.exports = async (isPM2) => {
               if(mcu && mcu.list && mcu.list.length ) for( let mc of mcu.list) {
   
                 if(mc.gulaDarahPuasa > 0 && mc.gulaDarahPuasa < 130 ) {
-                  console.log('')
-                  console.log('-------------')
-                  console.log('is DM: ', JSON.stringify(re))
+                  // console.log('')
+                  // console.log('-------------')
+                  // console.log('is DM: ', JSON.stringify(re))
                   console.log('is controlled: ', JSON.stringify(mc))
 
                   app.config.ARANGODB_DB ? pesertaArr = await app.arangoQuery({
