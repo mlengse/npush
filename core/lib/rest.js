8f760b18d87a26b29646889c6a40ce193a024bfe
mlengse <anjangkn@gmail.com>
2021-05-24 14:12:46 +0700

    asdad

--- core/lib/rest.js
+++ core/lib/rest.js
@@ -125,7 +125,7 @@ exports._getMCU = async ({
       })
 
       let res = await instance.get(`/mcu/kunjungan/${noKunjungan}`)
-      console.log(JSON.stringify(res))
+      console.log(res)
       if (res && res.data && res.data.response && res.data.response.count) {
         await that.arangoUpsert({
           coll: 'mcu',
